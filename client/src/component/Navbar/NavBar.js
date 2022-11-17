f38e6a9a57a97757361c1fdf354d1e7de690ff18
lee suyoen <dudgk0216@naver.com>
2022-11-17 16:35:25 +0900

    네브바 수정

--- client/src/component/Navbar/NavBar.js
+++ client/src/component/Navbar/NavBar.js
@@ -1,48 +1,45 @@
-import React from 'react';
-import styled from 'styled-components';
-
+import React from "react";
+import styled from "styled-components";
 
 const MenuItems = [
-  {"title":"Home","param":"/"},  {"title":"Register","param":"/register"}, 
-  {"title":"Login","param":"/login"}, {"title":"입양등록","param":"/upload"},
-  {"title":"입양후기","param":"/review"}, {"title":"입양신청","param":"/searchpet"},
-  {"title":"공지사항","param":"/notice"}, {"title":"MyPage","param":"/mypage"}
-]
+  { title: "Home", param: "/" },
+  { title: "Register", param: "/register" },
+  { title: "Login", param: "/login" },
+  { title: "입양게시판", param: "/upload" },
+  { title: "공지사항", param: "/notice" },
+  { title: "MyPage", param: "/mypage" },
+];
 
 function NavBar() {
   return (
-    <NavBarContainer >
-      {
-        MenuItems.map((item,index)=>{
-          return(
-            <a href={item.param}> <NavBarItem> {item.title}</NavBarItem></a>    
-          )
-        })
-      }
-       
-       
+    <NavBarContainer>
+      {MenuItems.map((item, index) => {
+        return (
+          <a href={item.param}>
+            {" "}
+            <NavBarItem> {item.title}</NavBarItem>
+          </a>
+        );
+      })}
     </NavBarContainer>
-  )
+  );
 }
 
-    const NavBarContainer = styled.div`
-    padding:10px;
-    width:100%;
-    height:40px;
-    display:flex;
-    justify-content:space-around;
-    border-bottom:1px solid black;
-    a{
-      text-decoration:none;
-      color:black;
-    }
-    
+const NavBarContainer = styled.div`
+  padding: 10px;
+  width: 100%;
+  height: 40px;
+  display: flex;
+  justify-content: space-around;
+  border-bottom: 1px solid black;
+  a {
+    text-decoration: none;
+    color: black;
+  }
+`;
+const NavBarItem = styled.div`
+  width: 100px;
+  height: 40px;
+`;
 
-    `
-    const NavBarItem =styled.div`
-
-      width:100px;
-      height:40px;
-    `
-
-export default NavBar
+export default NavBar;
