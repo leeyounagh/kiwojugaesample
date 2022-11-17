e7744c23deae51e956202f57d2d317de3be42181
lee suyoen <dudgk0216@naver.com>
2022-11-17 18:48:36 +0900

    캐러셀 구현

--- client/src/component/Main/Main.js
+++ client/src/component/Main/Main.js
@@ -1,14 +1,21 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import axios from "axios";
-import { Radio } from "antd";
-import { Collapse } from "antd";
-const { Panel } = Collapse;
+import Slider from "react-slick";
+
 const mainUrl = `http://api.visitjeju.net/vsjApi/contents/searchList?apiKey=sbrr93ynwcggx6br&locale=kr&category=c3`;
 
 function Main() {
   // 테스트 데이터 정의
   const [mainData, setMainData] = useState([]);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
 
   useEffect(() => {
     axios
@@ -24,7 +31,20 @@ function Main() {
 
   return (
     <MainContainer>
-      {/* 
+      <MainCarouselDiv>
+        <Slider {...settings}>
+          <MainCarouselImgDiv>
+            <img
+              src="https://i.pinimg.com/564x/56/70/25/567025f65043b80f34be3d862229e7e3.jpg"
+              alt="강아지"
+            ></img>
+          </MainCarouselImgDiv>
+          <MainCarouselImgDiv>
+            <img src="고양이들.jpg" alt="고양이"></img>
+          </MainCarouselImgDiv>
+        </Slider>
+      </MainCarouselDiv>
+
       <MainContentsContainer>
         {mainData.map((item, index) => {
           return (
@@ -40,26 +60,26 @@ function Main() {
             </>
           );
         })}
-      </MainContentsContainer> */}
+      </MainContentsContainer>
     </MainContainer>
   );
 }
+const MainCarouselImgDiv = styled.div`
+  img {
+    width: 80vw;
+    height: 80vh;
+  }
+`;
 const MainContainer = styled.div`
   width: 100%;
   margin-top: 50px;
 `;
-const MainTitleContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  width: 100%;
-  height: 30vh;
-  font-size: 5rem;
-`;
-const MainRadioBoxContainer = styled.div`
+const MainCarouselDiv = styled.div`
+  width: 80vw;
+  height: 80vh;
   margin-left: 10vw;
-  margin-bottom: 5vh;
+  margin-bottom: 100px;
 `;
-const MainRadioBoxLabel = styled.label``;
 const MainContentsContainer = styled.div`
   margin-left: 10vw;
 `;
